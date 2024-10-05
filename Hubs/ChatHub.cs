ae51f6abda66a5d5eca451e4dddc2e55856fc9f0
Norvez <normanalvarado123@gmail.com>
2024-10-05 16:28:56 -0600

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        // Enviar el mensaje a todos los clientes conectados
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
